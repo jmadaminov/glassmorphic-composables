e4fc7dfbf481028fb59e3dad53d020dcc68abde6
jahongir9779 <jahongir9779@gmail.com>
2022-05-16 16:20:51 +0500

    GlassmorphicColumn added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,21 +1,14 @@
 package com.example.myapplication
 
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -23,32 +16,33 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.geometry.RoundRect
-import androidx.compose.ui.graphics.ClipOp
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asAndroidBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.toSize
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import dev.shreyaspatil.capturable.Capturable
+import dev.shreyaspatil.capturable.controller.rememberCaptureController
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.withContext
+import uz.uzkassa.apay.glassmorphic_composables.GlassmorphicColumn
+import uz.uzkassa.apay.glassmorphic_composables.GlassmorphicRow
+import uz.uzkassa.apay.glassmorphic_composables.Place
+import uz.uzkassa.apay.glassmorphic_composables.fastblur
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -72,28 +66,14 @@ fun Greeting(name: String) {
     var screenWidthPx: Int
     var screenWidthDp: Int
     var cardWidthDp: Int
-    val cardMarginDp = 15
-
+    val context = LocalContext.current
     with(LocalDensity.current) {
         screenWidthDp = LocalConfiguration.current.screenWidthDp
         screenWidthPx = screenWidthDp.dp.toPx().toInt()
         cardWidthDp = screenWidthDp / 2
     }
 
-
-    val bg = Bitmap.createScaledBitmap(
-        BitmapFactory.decodeResource(
-            LocalContext.current.resources,
-            R.drawable.samarkand
-        ), screenWidthPx, screenWidthPx, true
-    )
-    val bgBlurred = Bitmap.createScaledBitmap(
-        BitmapFactory.decodeResource(
-            LocalContext.current.resources,
-            R.drawable.samarkand_dimmed_blurred
-        ), screenWidthPx, screenWidthPx, true
-    )
-
+    var capturedBitmap by remember { mutableStateOf<Bitmap?>(null) }
 
     val scrollState = rememberScrollState()
     val items = arrayListOf<Int>()
@@ -103,30 +83,48 @@ fun Greeting(name: String) {
 
     Box {
 
-        Image(
-            bitmap = bg.asImageBitmap(),
-            contentDescription = "",
-            modifier = Modifier.fillMaxWidth()
-        )
+        val captureController = rememberCaptureController()
+        Capturable(
+            controller = captureController,
+            onCaptured = { bitmap, error ->
+                // This is captured bitmap of a content inside Capturable Composable.
+                if (bitmap != null) {
+                    capturedBitmap = fastblur(bitmap.asAndroidBitmap(), 1f, 100)
+                    // Bitmap is captured successfully. Do something with it!
+                }
+
+                if (error != null) {
+                    // Error occurred. Handle it!
+                }
+            }
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.bg_autumn),
+                contentDescription = "",
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.FillWidth
+            )
+        }
+
+        LaunchedEffect(key1 = true, block = {
+            withContext(Main) {
+                captureController.capture()
+            }
+        })
+
 
         val childMeasures = remember {
             mutableStateListOf<Place>()
         }
-        var containerMeasures by remember { mutableStateOf<Place?>(null) }
         childMeasures.addAll(items.map { Place() })
 
-
-        GlassmorphicView(scrollState, containerMeasures, items, childMeasures, bgBlurred){
-            Row(
-                modifier = Modifier
-                    .horizontalScroll(scrollState)
-                    .onGloballyPositioned {
-                        if (containerMeasures == null) {
-                            containerMeasures = Place(it.size, it.positionInParent())
-                        }
-                    },
-                horizontalArrangement = Arrangement.spacedBy(cardMarginDp.dp),
-            ) {
+        capturedBitmap?.let {
+            GlassmorphicColumn(
+                modifier = Modifier.padding(horizontal = 15.dp),
+                scrollState, items, childMeasures, it,
+                dividerSpace = 10
+            )
+            {
                 Spacer(modifier = Modifier.width(10.dp))
                 items.forEachIndexed { index, it ->
                     Box(
@@ -135,91 +133,22 @@ fun Greeting(name: String) {
                                 childMeasures[index] = Place(it.size, it.positionInParent())
                             }
                             .width(cardWidthDp.dp)
-//                        .background(Color.Red)
+                            //                                .background(Color(0xFFFF0000))
                             .padding(15.dp)
-
                     ) {
-                        Text("Item $it")
+                        Text(
+                            "Item $it",
+                            color = Color.White
+                        )
                     }
                 }
                 Spacer(modifier = Modifier.width(10.dp))
-                3
-
             }
 
         }
 
 
-
     }
-
-}
-
-@Composable
-private fun GlassmorphicView(
-    scrollState: ScrollState,
-    containerMeasures: Place?,
-    items: ArrayList<Int>,
-    childMeasures: SnapshotStateList<Place>,
-    bgBlurred: Bitmap,
-    content : @Composable () ->Unit
-) {
-    Canvas(
-        modifier = Modifier
-            .horizontalScroll(scrollState)
-            .width(containerMeasures?.size?.width?.dp ?: 0.dp)
-            .height(containerMeasures?.size?.height?.dp ?: 0.dp)
-    ) {
-        for (i in items.indices) {
-            val path = Path()
-
-            path.addRoundRect(
-                RoundRect(
-                    Rect(
-                        offset = childMeasures[i].offset,
-                        size = childMeasures[i].size.toSize(),
-                    ),
-                    CornerRadius(10.dp.toPx())
-                )
-            )
-
-            clipPath(path, clipOp = ClipOp.Intersect) {
-                drawImage(
-                    bgBlurred.asImageBitmap(),
-                    Offset(scrollState.value.toFloat(), 0f)
-                )
-            }
-
-
-        }
-
-    }
-    content()
-
-}
-
-@Immutable
-data class Place(var size: IntSize = IntSize(0, 0), var offset: Offset = Offset(0f, 0f)) {
-
-}
-
-
-@Composable
-fun MyBlurLayout(content: @Composable () -> Unit) {
-
-
-//    Layout(content = content, modifier = Modifier) { measurables, constraints ->
-//
-//
-//        val itemWidth = constraints.maxWidth / 10
-//        val itemConstraints = constraints.copy(minWidth = itemWidth, maxWidth = itemWidth)
-//
-//        val placeables = measurables.map {
-//            it.measure(itemConstraints)
-//        }
-//
-//
-//    }
 
 }
 
---
+++ glassmorphic-composables/src/main/java/uz/uzkassa/apay/glassmorphic_composables/GlassmorphicRow.kt
@@ -0,0 +1,211 @@
+package uz.uzkassa.apay.glassmorphic_composables
+
+import android.graphics.Bitmap
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.RoundRect
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.ClipOp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInParent
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
+
+
+@Composable
+fun GlassmorphicRow(
+    modifier: Modifier = Modifier,
+    scrollState: ScrollState,
+    items: ArrayList<Int>,
+    childMeasures: SnapshotStateList<Place>,
+    bgBlurred: Bitmap,
+    dividerSpace: Int = 10,
+    content: @Composable (RowScope.() -> Unit),
+) {
+
+    var containerMeasures by remember { mutableStateOf<Place?>(null) }
+
+    val strokeColor = Color(0x80ffffff)
+    val transparent = Color.Transparent
+    val t = containerMeasures?.size?.height?.dp?.let { parentDp ->
+        containerMeasures?.offset?.y?.toInt()?.dp?.let { childDp ->
+            parentDp + childDp
+        } ?: run { 0.dp }
+    } ?: run { 0.dp }
+    Canvas(
+        modifier = Modifier
+            .horizontalScroll(scrollState)
+            .width(containerMeasures?.size?.width?.dp ?: 0.dp)
+            .height(t)
+    ) {
+        for (i in items.indices) {
+            val path = Path()
+
+            path.addRoundRect(
+                RoundRect(
+                    Rect(
+                        offset = Offset(
+                            (containerMeasures?.offset?.x ?: 0f) + childMeasures[i].offset.x,
+                            (containerMeasures?.offset?.y ?: 0f) + childMeasures[i].offset.y
+                        ),
+                        size = childMeasures[i].size.toSize(),
+                    ),
+                    CornerRadius(10.dp.toPx())
+                )
+            )
+
+            clipPath(path, clipOp = ClipOp.Intersect) {
+                drawImage(
+                    bgBlurred.asImageBitmap(),
+                    Offset(scrollState.value.toFloat(), 0f)
+                )
+            }
+            drawPath(
+                path = path,
+                color = strokeColor,
+                style = Stroke(1f),
+                blendMode = BlendMode.Luminosity
+//                blendMode = BlendMode.Luminosity
+            )
+            drawPath(
+                path = path,
+                brush = Brush.verticalGradient(listOf(strokeColor, transparent)),
+                blendMode = BlendMode.Overlay
+//                blendMode = BlendMode.Plus
+//                blendMode = BlendMode.Screen
+//                blendMode = BlendMode.Luminosity
+            )
+
+
+        }
+
+    }
+
+    Row(
+        modifier = modifier
+            .horizontalScroll(scrollState)
+            .onGloballyPositioned {
+                if (containerMeasures == null) {
+                    containerMeasures = Place(it.size, it.positionInParent())
+                }
+            },
+        horizontalArrangement = Arrangement.spacedBy(dividerSpace.dp),
+    ) {
+        content()
+    }
+
+}
+
+@Composable
+fun GlassmorphicColumn(
+    modifier: Modifier = Modifier,
+    scrollState: ScrollState,
+    items: ArrayList<Int>,
+    childMeasures: SnapshotStateList<Place>,
+    bgBlurred: Bitmap,
+    dividerSpace: Int = 10,
+    content: @Composable (ColumnScope.() -> Unit),
+) {
+
+    var containerMeasures by remember { mutableStateOf<Place?>(null) }
+
+    val strokeColor = Color(0x80ffffff)
+    val transparent = Color.Transparent
+    val t = containerMeasures?.size?.width?.dp?.let { parentDp ->
+        containerMeasures?.offset?.x?.toInt()?.dp?.let { childDp ->
+            parentDp + childDp
+        } ?: run { 0.dp }
+    } ?: run { 0.dp }
+    Canvas(
+        modifier = Modifier
+            .verticalScroll(scrollState)
+            .width(t)
+            .height(containerMeasures?.size?.height?.dp ?: 0.dp)
+    ) {
+        for (i in items.indices) {
+            val path = Path()
+
+            path.addRoundRect(
+                RoundRect(
+                    Rect(
+                        offset = Offset(
+                            (containerMeasures?.offset?.x ?: 0f) + childMeasures[i].offset.x,
+                            (containerMeasures?.offset?.y ?: 0f) + childMeasures[i].offset.y
+                        ),
+                        size = childMeasures[i].size.toSize(),
+                    ),
+                    CornerRadius(10.dp.toPx())
+                )
+            )
+
+            clipPath(path, clipOp = ClipOp.Intersect) {
+                drawImage(
+                    bgBlurred.asImageBitmap(),
+                    Offset(0f,scrollState.value.toFloat())
+                )
+            }
+            drawPath(
+                path = path,
+                color = strokeColor,
+                style = Stroke(1f),
+                blendMode = BlendMode.Luminosity
+//                blendMode = BlendMode.Luminosity
+            )
+            drawPath(
+                path = path,
+                brush = Brush.verticalGradient(listOf(strokeColor, transparent)),
+                blendMode = BlendMode.Overlay
+//                blendMode = BlendMode.Plus
+//                blendMode = BlendMode.Screen
+//                blendMode = BlendMode.Luminosity
+            )
+
+
+        }
+
+    }
+
+    Column(
+        modifier = modifier
+            .verticalScroll(scrollState)
+            .onGloballyPositioned {
+                if (containerMeasures == null) {
+                    containerMeasures = Place(it.size, it.positionInParent())
+                }
+            },
+        verticalArrangement = Arrangement.spacedBy(dividerSpace.dp),
+    ) {
+        content()
+    }
+
+}
+
+@Immutable
+data class Place(var size: IntSize = IntSize(0, 0), var offset: Offset = Offset(0f, 0f))
